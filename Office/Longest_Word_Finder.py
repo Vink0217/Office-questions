94e7e8893c82595daef466ba8d5964ed4393a850
Vinayak <157098004+Vink0217@users.noreply.github.com>
2025-05-27 12:58:58 +0530

    Delete Office directory

--- Office/Longest_Word_Finder.py
+++
@@ -1,8 +0,0 @@
-'''
-Q7) Longest Word Finder 
-Given a sentence, find and print the longest word. 
-'''
-string_input = str(input("Enter String: "))
-word = string_input.split()
-longest_word = max(word, key=len)    
-print(longest_word)
